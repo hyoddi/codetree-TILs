cafba1e87ee50bca37ae1701d4571962322167fa
hyoddi <110017938+hyoddi@users.noreply.github.com>
2024-03-10 14:49:18 +0900

    Title: 공백을 기준으로 출력, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240310/공백을 기준으로 출력/output-based-on-space.py
@@ -0,0 +1,12 @@
+# 주어진 두 줄의 문자열 입력 받기
+line1 = input("첫 번째 줄을 입력하세요: ")
+line2 = input("두 번째 줄을 입력하세요: ")
+
+# 두 줄의 문자열을 하나의 문자열로 합치기
+combined_string = line1 + " " + line2
+
+# 합쳐진 문자열에서 공백 제거하기
+result = combined_string.replace(" ", "")
+
+# 결과 출력
+print(result)
