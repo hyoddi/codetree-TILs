c88aff78956caaa09130d10373a0e6b834a761c9
hyoddi <110017938+hyoddi@users.noreply.github.com>
2024-04-08 19:44:33 +0900

    Title: 대문자로 출력하기, Time: 161ms, Memory: 24MB, Status: Passed - CodeTree

--- 240408/대문자로 출력하기/print-in-capital.py
+++ 240408/대문자로 출력하기/print-in-capital.py
@@ -1,9 +1,10 @@
 arr = input()
 ans = ''
 
-list(arr)
+# print(ord('a') - ord('A')) = 32, 즉 소문자에 32를 빼면 대문자가 나온다.
+
 for i in arr:
-    if i.isalpha() == True:
-        ans += i.upper()
+    if (i >= 'a' and i <= 'z') or (i >='A' and i<= 'Z'):
+        ans+=i.upper()
 
 print(ans)
