b4275787303ad6282e10042ae3d561d0174ab6cc
hyoddi <110017938+hyoddi@users.noreply.github.com>
2024-04-13 15:46:41 +0900

    Title: 방향에 맞춰 이동, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240413/방향에 맞춰 이동/move-in-direction.py
+++ 240413/방향에 맞춰 이동/move-in-direction.py
@@ -1,26 +1,29 @@
-N = int(input())
+# 변수 선언 및 입력
+n = int(input())
+x, y = 0, 0
 
-nx = 0
-ny = 0
+# 동, 서, 남, 북 순으로 dx, dy를 정의합니다.
+dx = [1, -1,  0, 0]
+dy = [0,  0, -1, 1]
 
-def op(w, n):
-    global dx, dy, nx, ny
-    if w == 'E':
-        nx += n
+# 움직이는 것을 진행합니다.
+for _ in range(n):
+    c_dir, dist = tuple(input().split())
+    dist = int(dist)
+    
+    # 각 방향에 맞는 번호를 붙여줍니다.
+    if c_dir == 'E':
+        move_dir = 0
+    elif c_dir == 'W':
+        move_dir = 1
+    elif c_dir == 'S':
+        move_dir = 2
+    else:
+        move_dir = 3
 
-    elif w == 'S':
-        ny -= n
-
-    elif w == 'W':
-        nx -= n
-
-    elif w == 'N':
-        ny += n
-
-
-for _ in range(N):
-    way, num = map(str, input().split())
-    num = int(num)
-    op(way, num)
-
-print(nx, ny)
+    # 주어진 방향대로 dist 거리만큼 이동했을 경우의
+    # 위치를 구해줍니다.
+    x += dx[move_dir] * dist
+    y += dy[move_dir] * dist
+    
+print(x, y)
