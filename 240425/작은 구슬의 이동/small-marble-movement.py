30bbea921fae00bfc23beae8225be93665d3ff58
hyoddi <110017938+hyoddi@users.noreply.github.com>
2024-04-25 20:25:45 +0900

    Title: 작은 구슬의 이동, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240425/작은 구슬의 이동/small-marble-movement.py
+++ 240425/작은 구슬의 이동/small-marble-movement.py
@@ -2,36 +2,39 @@ n, t = map(int, input().split())
 r, c, d = map(str, input().split())
 r ,c = int(r), int(c)
 
-# 
-dxs = [0, 1, -1, 0]
-dys = [1, 0, 0, 1]
+# 3 - 현재 방향을 빼면 뒤집힐 수 있도록 설정
+
+dys = [1, 0, 0, -1] # 행, x값
+dxs = [0, 1, -1, 0] # 열, y값
 
 
 # 범위 안에 있으면 True, 없으면 False 반환
 def check(x, y):
-    return 0 <= x and x < n and 0 <= y and y < n
+    return x > 0 and x <= n  and y > 0 and y <= n
 
 
 # 딕셔너리 이용
 mapper = {
     'R': 0,
-    'D': 1,
-    'U': 2,
+    'U': 1,
+    'D': 2,
     'L': 3
 }
 
+# 방향 초기값 설정 
 way = mapper[d]
-
-
-arr = [[0 for _ in range(n)] for _ in range(n)]
 
 for _ in range(t):
 
-    nx, ny = r + dxs[way], c + dys[way]
+    ny = r + dxs[way] # 행
+    nx = c + dys[way] # 열
+
     if not check(nx, ny):
         way = 3 - way
-        
+    
     else:
-        r, c = nx, ny 
+        r, c = ny, nx
+    
+    
 
 print(r, c)
