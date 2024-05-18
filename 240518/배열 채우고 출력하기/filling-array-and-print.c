2578b554296c15f58bc4187d01e887355782b0a4
hyoddi <110017938+hyoddi@users.noreply.github.com>
2024-05-18 15:52:15 +0900

    Title: 배열 채우고 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240518/배열 채우고 출력하기/filling-array-and-print.c
+++ 240518/배열 채우고 출력하기/filling-array-and-print.c
@@ -5,11 +5,11 @@ int main() {
     char arr[10];
 
     for(int i = 0; i < 10; i++){
-        scanf("%c", &arr[i]);
+        scanf("%c ", &arr[i]);
     }
 
-    for(int i = 9; i >=0; i--){
-        printf("%c ", arr[i]);
+    for(int i = 9; i >= 0; i--){
+        printf("%c", arr[i]);
     }
 
     return 0;
